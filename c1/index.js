f369449de8d29c968c1d450c939b26dc71fa3e44
AnaKjurchievska <43668869+AnaKjurchievska@users.noreply.github.com>
2019-06-12 22:06:20 +0200

    cas-1

---
+++ c1/index.js
@@ -0,0 +1,17 @@
+console.log('Hello!');
+
+ime = 'Niki';
+var prezime = 'Grunoski';
+let vozrast = 23;
+
+console.log(ime);
+
+
+
+function test(){
+    console.log(ime);
+    console.log(prezime);
+    console.log(vozrast);
+}
+
+test();
