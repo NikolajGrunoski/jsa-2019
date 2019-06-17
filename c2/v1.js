5733bf55052cd7f3bc4d2be022cebb63e35ab3f3
AnaKjurchievska <43668869+AnaKjurchievska@users.noreply.github.com>
2019-06-17 22:06:44 +0200

    cas 2

---
+++ c2/v1.js
@@ -0,0 +1,10 @@
+var func = require('./v2');
+
+
+func.everyThird();
+
+//everyThird(4);
+
+var r = func.arrSum([248,21,95,239,46]);
+console.log(r);
+
---
+++ c2/v2.js
@@ -0,0 +1,56 @@
+function everyThird(){
+    var i = 0;
+    var b = 0;
+    while(i < 100){
+        i++;
+
+        if(i % 2 == 0){
+            b++;                         
+        }
+
+        if(i % 2 == 0 && b == 3){
+            console.log(i);
+            b = 0;
+        }
+    }
+}
+
+// function everyThird(c){
+//     var i = 0;
+//     var b = 0;
+//     while(i < 100){
+//         i++;
+
+//         if(i % 2 == 0){
+//             b++;
+//         }
+
+//         if(i % 2 == 0 && b == c){
+//             console.log(i);
+//             b = 0;
+//         }
+//     }
+// }
+
+//Vtora zadaca
+
+function arrSum(a){
+    var c = 0;
+    for(var i = 0; i < a.lenght; i++){
+        if(a[i].toString().lenght > 1){
+            c++;
+            a[i] = a[i].toString().split('').reduce((a,b) => parseInt(a) + parseInt(b));
+        }
+    }
+    if(c > 0){
+        return arrSum(a);
+    }else{
+        return a.reduce((a,b) => a + b);
+    }
+}
+
+
+module.exports = {
+    arrSum,
+    everyThird
+};
