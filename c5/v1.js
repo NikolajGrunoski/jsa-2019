fd5a5452f45ba666b131e7586e22238c1c4f94f1
Nikolaj Grunoski <nikolaj.grunoski@gmail.com>
2019-06-26 22:02:58 +0200

    cas 5

---
+++ c5/v1.js
@@ -0,0 +1,15 @@
+var http = require ('http');
+
+var server = http.createServer(function(req,res){
+    res.write(`Method: ${req.method}, URL: ${req.url}`);
+    res.end();
+})
+
+server.listen(8080, (err) =>{
+    if(err){
+        console.error(err);
+        return;
+    }
+    console.log('Server started successfully');
+});
+
