7de97569fb46f243f39e6f7fa71f5a1b5e22c7a1
NikolajGrunoski <nikolaj.grunoski@gmail.com>
2019-10-02 21:48:36 +0200

    7

--- React/my-app/src/Wrapper.js
+++ React/my-app/src/Wrapper.js
@@ -3,48 +3,48 @@ import User from './User'
 import axios from 'axios'
 import Error from "./Error"
 
-class Wrapper extends React.Component{
+class Wrapper extends React.Component {
     // let users = []
     // users = data.map(element => {
     //     return <User key={element.id} name={element.name} email={element.email} /> 
     // })
 
-    constructor(props){
+    constructor(props) {
         super(props)
         this.state = {
             data: null
         }
     }
 
-    componentDidMount () {
+    componentDidMount() {
         axios.get('https://jsonplaceholder.typicode.com/users')
             .then((response) => {
                 const users = response.data.map((element) => {
-                    return <User 
-                    key={element.id} 
-                    name={element.name} 
-                    email={element.email} />
+                    return <User
+                        key={element.id}
+                        name={element.name}
+                        email={element.email} />
                 })
 
-                this.setState({data: users})
+                this.setState({ data: users })
             })
             .catch((error) => {
-                this.setState({data : <Error/>})
+                this.setState({ data: <Error /> })
             })
     }
 
-    render () {
+    render() {
         return (
             <React.Fragment>
                 {this.props.children}
                 {
                     !this.state.data
-                    ? <h6> LOADING ...</h6>
-                    : this.state.data
+                        ? <h6> LOADING ...</h6>
+                        : this.state.data
                 }
             </React.Fragment>
         )
-        
+
     }
 }
 
