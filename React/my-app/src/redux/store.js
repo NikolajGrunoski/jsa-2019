581ac9a8e2f9bfbac5f4ee0ae122e7eb5c9ec0c8
NikolajGrunoski <nikolaj.grunoski@gmail.com>
2019-10-21 22:02:25 +0200

    new

--- React/my-app/src/redux/store.js
+++ React/my-app/src/redux/store.js
@@ -1,11 +1,13 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import logger from 'redux-logger'
 import { userReducer } from './reducers/userReducer.js'
+import { usersTableReducers } from './reducers/usersTableReducers.js'
 
 
 
 const singleReducer = combineReducers({
     userReducer,
+    usersTableReducers,
 })
 
 // Creates an empty store object = {}
